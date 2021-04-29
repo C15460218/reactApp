dc2b9ec338bd35a46e04428a2c785ec212a19c4d
Adrian Robles <C15460218@colima.tecnm.mx>
2021-04-28 19:03:25 -0500

    20210428 Navigation Commit

--- App.js
+++ App.js
@@ -7,6 +7,8 @@
  */
 
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { View, Text, Image, Button, Alert } from 'react-native';
 
 const styles = {
@@ -21,13 +23,20 @@ const styles = {
     resizeMode: 'cover'
   },
   ctaButton: {
-    backgroundColor: 'dodgerblue ',
+    backgroundColor: 'dodgerblue',
+    color: 'white',
+    borderRadius: 100
+  },
+  bckButton: {
+    backgroundColor: 'red',
     color: 'white',
     borderRadius: 100
   }
+
+
 }
 
-const App = () => {
+const Inicio = (prop) => {
   return(
     <View style = {{flex: 1}}>
         <View style = {{flexGrow: 1,alignItems: 'center', justifyContent: 'center'}}>
@@ -38,8 +47,34 @@ const App = () => {
         <Text style = {{color: 'darkcyan'}}>Adrian Manuel Robles Jiménez</Text>
         <Text>MARZO - JUNIO 2021</Text>
       </View>
-      <Button style={styles.ctaButton} title="Entrar" onPress={()=>Alert.alert("ESP: Aplicacines Multiplataforma")}/>
+      <Button style={styles.ctaButton} title="Entrar" onPress={()=>prop.navigation.navigate('segunda')}/>
     </View>
+  );
+};
+
+const SegundaPantalla = (prop) => {
+  return(
+    <View style = {{flex: 1}}>
+      <Text>Segunda Pantalla!</Text>
+      <Button style={styles.bckButton} title="Atras" onPress={()=>prop.navigation.goBack()}/>
+      <Button style={styles.bckButton} title="Ir a inicio" onPress={()=>prop.navigation.navigate('inicio')}/>
+      <Button style={styles.bckButton} title="Ir a segunda" onPress={()=>prop.navigation.navigate('segunda')}/>
+      <Button style={styles.bckButton} title="Ir a segunda (forzado)" onPress={()=>prop.navigation.push('segunda')}/>
+      <Button style={styles.bckButton} title="Regresar al principio" onPress={()=>prop.navigation.popToTop()}/>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="inicio" component ={Inicio} options={{title: "Página Principal"}} />
+        <Stack.Screen name="segunda" component ={SegundaPantalla} options={{title: "Página Secundaria"}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
